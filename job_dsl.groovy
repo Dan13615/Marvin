4ef03f0cff03d770f6b0006bc56ddcad42175b03
Dan13615 <daniil.stepanov@epitech.eu>
2024-07-16 16:41:46 +0200

    [+] jobs done

---
+++ job_dsl.groovy
@@ -0,0 +1,65 @@
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+}
+
+freeStyleJob('Tools/clone-repository') {
+    description('Clones a repository.')
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    wrappers {
+        preBuildCleanup {
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('git clone ${GIT_REPOSITORY_URL}')
+    }
+}
+
+freeStyleJob('/Tools/SEED') {
+    parameters {
+        stringParam("GITHUB_NAME", "", "GitHub repository owner/repo_name (e.g.: \"EpitechIT31000/chocolatine\")")
+        stringParam("DISPLAY_NAME", "", "Display name for the job")
+    }
+    wrappers {
+        preBuildCleanup {
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        dsl {
+            scriptText = '''
+                job(DISPLAY_NAME) {
+                    scm {
+                        git {
+                            remote {
+                                github(GITHUB_NAME)
+                            }
+                        }
+                    }
+                    wrappers {
+                        preBuildCleanup { // Clean before build
+                            includePattern('**/target/**')
+                            deleteDirectories()
+                            cleanupParameter('CLEANUP')
+                        }
+                    }
+                    steps {
+                        shell('make fclean')
+                        shell('make')
+                        shell('make tests_run')
+                        shell('make clean')
+                    }
+                    triggers {
+                        scm('*/1 * * * *')
+                    }
+                }
+                '''
+        }
+    }
+}
